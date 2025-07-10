73232183076435ff2f938f0cb9a8ba76e412f031
shivaaa07 <shivaawork07@gmail.com>
2025-07-10 11:52:10 +0530

    Started Python Learning

---
+++ getting_started.py
@@ -0,0 +1,23 @@
+print(2 + 3)
+
+print(8 + 9 - 10)
+
+print(3 * 4)
+
+print(8 / 4)    # Floating Division
+
+print(10 // 3)  # Integer Division
+
+print(10 % 3)
+
+print("Shiva")
+
+print("Shiva's")
+
+print("Shiva's laptop")
+
+print('Shiva\'s "Laptop"')
+
+print("d:\docs\navin's")
+
+print(r"d:\docs\navins")
