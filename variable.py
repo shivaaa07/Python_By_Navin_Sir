9ad466845b8fdb34f39ddc5efacac545052d0a01
shivaaa07 <shivaawork07@gmail.com>
2025-07-12 22:28:13 +0530

    multiline variable learning

--- variable.py
+++ variable.py
@@ -1,10 +1,32 @@
-number1 = 2
-number2 = 10
+user_name = "Shiva Sahani"
 
-print(number1 + number2)
+fruit_one, fruit_two, fruit_three = "Mango", "Banana", "Kiwi"
+# print(fruit_one, fruit_two, fruit_three)
 
-# print(_ + number2)       Only work on Python shell
+"""
+Multi Line Comments
+num1 = 10
+num2 = 20
+dummy = num2
+num2 = num1
+num1 = dummy
+"""
 
-name = "YouTube"
+num1 = 10
+num2 = 20
 
-print(len(name))
+num1, num2 = num2, num1     # Multi Line Variable Define
+
+# print("The Value of num1", num1)
+# print("The Value of num2", num2)
+
+# Global Variable
+
+def msg():
+    var1 = "World"
+    print("Hello " + user_name)
+    print(var1)
+msg()
+
+print(var1)
+
